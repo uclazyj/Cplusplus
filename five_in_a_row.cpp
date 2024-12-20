188abd4da10e94db78863a3d73e004737b00a8ea
yujianzhao <zhaoyujian@ucla.edu>
2024-12-20 15:29:46 -0500

    Finish a playable version.

--- five_in_a_row.cpp
+++ five_in_a_row.cpp
@@ -7,15 +7,25 @@ const int MAX_ROWS = 19;
 const int MAX_COLS = 19;
 
 class Player {
+public:
+    Player(string name, char token):name_(name),token_(token){}
+    char get_token(){return token_;}
+    string get_name(){return name_;}
 private:
     string name_;
     char token_;
-    bool win_;
 };
 
 class Game {
 public:
-    Game(int nRows, int nCols):board_(nRows, vector<char>(nCols, '.')), nRows_(nRows), nCols_(nCols){}
+    Game(int nRows, int nCols, string name1, char token1, string name2, char token2):
+    board_(nRows, vector<char>(nCols, '.')), 
+    nRows_(nRows), nCols_(nCols){
+        Player player1 = Player(name1, token1);
+        Player player2 = Player(name2, token2);
+        players_.push_back(player1);
+        players_.push_back(player2);
+    }
 
     void print_board(){
         cout << endl;
@@ -32,6 +42,123 @@ public:
             cout << endl;
         }
         cout << endl;
+    }
+
+    bool play(int row, int col, Player player){
+        char token = player.get_token();
+        board_[row][col] = token;
+        print_board();
+        // win from horizontal
+        {
+            int length = 1;
+            for (int c = col + 1; c < nCols_; ++c){
+                if (board_[row][c] == token){
+                    length++;
+                }
+                else{
+                    break;
+                }
+            }
+            for (int c = col - 1; c >= 0; --c){
+                if (board_[row][c] == token){
+                    length++;
+                }
+                else{
+                    break;
+                }
+            }
+            if (length >= 5){
+                return true;
+            }
+        }
+        // win from vertical
+        {
+            int length = 1;
+            for (int r = row + 1; r < nRows_; ++r){
+                if (board_[r][col] == token){
+                    length++;
+                }
+                else{
+                    break;
+                }
+            }
+            for (int r = row - 1; r >= 0; --r){
+                if (board_[r][col] == token){
+                    length++;
+                }
+                else{
+                    break;
+                }
+            }
+            if (length >= 5){
+                return true;
+            }
+        }
+        // win from up right diagonal
+        {
+            int length = 1;
+            // up right
+            for (int i = 1; col + i < nCols_ && row - i >= 0; ++i){
+                if (board_[row - i][col + i] == token){
+                    length++;
+                }
+                else{
+                    break;
+                }
+            }
+            // left down
+            for (int i = 1; col - i >= 0 && row + i < nRows_; ++i){
+                if (board_[row + i][col - i] == token){
+                    length++;
+                }
+                else{
+                    break;
+                }
+            }
+            if (length >= 5){
+                return true;
+            }
+        }
+        // win from right down diagonal
+        {
+            int length = 1;
+            // up left
+            for (int i = 1; row - i >= 0 && col - i >= 0; ++i){
+                if (board_[row - i][col - i] == token){
+                    length++;
+                }
+                else{
+                    break;
+                }
+            }
+            // left down
+            for (int i = 1; row + i < nRows_ && col + i < nCols_; ++i){
+                if (board_[row + i][col + i] == token){
+                    length++;
+                }
+                else{
+                    break;
+                }
+            }
+            if (length >= 5){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void start(){
+        while (true) {
+            // Player 1 plays
+            for (int i = 0; i < players_.size(); ++i){
+                cout << players_[i].get_name() << "'s turn (" << players_[i].get_token() << "):" << endl;
+                pair<int, int> coord = get_coord();
+                if (play(coord.first, coord.second, players_[i])){
+                    cout << "Congratulations " << players_[i].get_name() << ", you win!" << endl;
+                    return;
+                }
+            } 
+        }
     }
 
     // Return the index of row and column
@@ -52,18 +179,20 @@ public:
 
 private:
     vector<vector<char>> board_;
+    vector<Player> players_;
     const int nRows_;
     const int nCols_;
-
 };
 
 int main(){
     // Ideally comes from user input
     int nRows = 9;
     int nCols = 9;
-    Game game = Game(nRows, nCols);
+    string name1 = "Alice";
+    char token1 = 'X';
+    string name2 = "Bob";
+    char token2 = 'O';
+    Game game = Game(nRows, nCols, name1, token1, name2, token2);
     game.print_board();
-    pair<int,int> coord = game.get_coord();
-    cout << "(" << coord.first << "," << coord.second << ")" << endl;
-
+    game.start();
 }
