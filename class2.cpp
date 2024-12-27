4a941ee36e3bf076b6ee9120e0e53e4443960e06
yujianzhao <zhaoyujian@ucla.edu>
2024-12-27 10:36:16 -0500

    Add demonstration for polymorphism

--- class2.cpp
+++ class2.cpp
@@ -5,10 +5,16 @@ using namespace std;
 class Animal {
 public:
     Animal(int weight):weight_(weight){}
+
     virtual void Greet(){
         cout << "I am an animal!" << endl;
     } 
+
     int get_weight(){return weight_;}
+
+    void SayHi(){
+        Greet();
+    }
 private:
     int weight_;
 };
@@ -23,10 +29,26 @@ private:
     bool cute_;
 };
 
-int main() {
-    Animal a = Animal(5);
+// polymorphism only works with base pointer or base reference
+
+void Greet_with_base_pointer(Animal* a){
+    a->Greet();
+}
+
+void Greet_with_base_reference(Animal& a){
     a.Greet();
-    Dog dog = Dog();
-    dog.Greet();
-    cout << dog.get_weight() << endl;
+}
+
+// Chopping happens
+void Greet_with_base_copy(Animal a){
+    a.Greet();
+}
+
+int main() {
+    Animal* a = new Dog;
+    Greet_with_base_pointer(a);
+    Greet_with_base_reference(*a);
+    Greet_with_base_copy(*a);
+
+    a->SayHi();
 }
